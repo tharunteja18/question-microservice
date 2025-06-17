4d48f61f50f7a4d98655f6e9a3c8f3c5890261d0
tharunteja18 <tharunteja114@gmail.com>
2025-06-17 22:52:16 +0530

    Added Swagger dependency for API documentation. and Created `SwaggerConfiguration` class to define basic API info (title, version, description).

---
+++ src/main/java/com/nerchuko/questionservice/config/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.nerchuko.questionservice.config;
+
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customeOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                .title("Question Service API")
+                        .version("1.0")
+                        .description("Question Service API Information"));
+    }
+
+}
